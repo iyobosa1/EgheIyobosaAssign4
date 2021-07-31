bfa803a818e909fef018d251ad521a3256ef28e4
iyobo2 <78611579+iyobo2@users.noreply.github.com>
2021-07-31 15:18:41 -0400

    1st commit. Project created with very few changes.

---
+++ EgheAssign44/src/main/java/eghe/iyobosa/n01107171/ei/EgheActivity.java
@@ -0,0 +1,14 @@
+package eghe.iyobosa.n01107171.ei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EgheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
