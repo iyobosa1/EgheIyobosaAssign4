3d0d33d33717b8c9634f5852c75825ad041e4382
iyobo2 <78611579+iyobo2@users.noreply.github.com>
2021-08-09 02:30:26 -0400

    Fixed the error. New task fix the drawer<to do>

--- EgheAssign44/src/main/java/eghe/iyobosa/n01107171/ei/EgheActivity.java
+++ EgheAssign44/src/main/java/eghe/iyobosa/n01107171/ei/EgheActivity.java
@@ -7,11 +7,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.pm.ActivityInfo;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.Nullable;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -19,20 +28,23 @@ import java.util.Calendar;
 
 public class EgheActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    Calendar calendar;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+
+        Toolbar toolbar;
+        private DrawerLayout drawerLayout;
+        Calendar calendar;
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eghe);
 
         toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//      setSupportActionBar(toolbar);
 
         NavigationView navigationView = findViewById(R.id.egheNav_view);
-
-       ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//       navigationView.setNavigationItemSelectedListener(this);
+       drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -48,22 +60,22 @@ public class EgheActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_menu,menu);
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.top_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        @Override
+        public boolean onOptionsItemSelected (@NonNull MenuItem item){
         Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         startActivity(browser);
         return super.onOptionsItemSelected(item);
     }
 
-    public void choiceAction(MenuItem item) {
-        Intent i = new Intent(getApplicationContext(),EgheActivity.class);
+        public void choiceAction (MenuItem item){
+        Intent i = new Intent(getApplicationContext(), EgheActivity.class);
         startActivity(i);
 
     }
@@ -71,23 +83,23 @@ public class EgheActivity extends AppCompatActivity {
 
 //    @Override
 //    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        switch (menuItem.getItemId()) {
-//            case R.id.nhungHome:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhHome()).commit();
-//                break;
-//            case R.id.nhungNhung:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhuDown()).commit();
+////        switch (menuItem.getItemId()) {
+////            case R.id.nhungHome:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhHome()).commit();
+////                break;
+////            case R.id.nhungNhung:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhuDown()).commit();
+////
+////                break;
+////            case R.id.nhungNguyen:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NgSrv()).commit();
+////                break;
+////            case R.id.nhungSetting:
+////                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhuSet()).commit();
+////                break;
 //
-//                break;
-//            case R.id.nhungNguyen:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NgSrv()).commit();
-//                break;
-//            case R.id.nhungSetting:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nhungFragment_container,new NhuSet()).commit();
-//                break;
-//
-//        }
-//        drawerLayout.closeDrawer(GravityCompat.START);
+//        //}
+////        drawerLayout.closeDrawer(GravityCompat.START);
 //        return true;
-//    }
-}
+//   }
+    }
